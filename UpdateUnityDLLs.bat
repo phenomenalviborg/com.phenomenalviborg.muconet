46ef9e9f38906daff632eb80e151131306f89fd9
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-03-21 12:44:28 +0100

    MUCONET-70

--- UpdateUnityDLLs.bat
+++
@@ -1,9 +0,0 @@
-del ".\MUCOUnityClient\Assets\MUCONet.dll"
-del ".\MUCOUnityClient\Assets\MUCONet.dll.meta"
-echo F|xcopy ".\MUCONet\bin\Debug\netcoreapp3.1\MUCONet.dll" ".\MUCOUnityClient\Assets\MUCONet.dll"
-
-del ".\MUCOUnityServer\Assets\MUCONet.dll"
-del ".\MUCOUnityServer\Assets\MUCONet.dll.meta"
-echo F|xcopy ".\MUCONet\bin\Debug\netcoreapp3.1\MUCONet.dll" ".\MUCOUnityServer\Assets\MUCONet.dll"
-
-pause
